f5af087b132d88b5049b1c50a13ab83cb4176242
kennpangilinan <kenn.pangilinan@gmail.com>
2016-02-17 17:22:00 -0800

    set host parameter for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,6 +12,7 @@ Rails.application.configure do
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
+  config.action_mailer.default_url_options = { host: 'drink-wheel.herokuapp.com' }
   config.action_controller.perform_caching = true
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
