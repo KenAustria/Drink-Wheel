91cb6361965fe6e708a89bcda5755df9e9e3cf0b
kennpangilinan <kenn.pangilinan@gmail.com>
2016-08-16 14:30:37 -0700

    Implement Momentum Scrolling and added Service/Contact section

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,6 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
-
-
 //set default degree (360*5)
 var degree = 1800;
 //number of clicks = 0
@@ -29,59 +27,57 @@ $(document).ready(function() {
     var newDegree = degree * clicks; //1800*1
     var extraDegree = Math.floor(Math.random() * (360 - 1 + 1)) + 1; // 99
     totalDegree = newDegree + extraDegree; // 1800 + 99 = 1899
-    revealChoice = setTimeout(function(){  // function executes ones after a specified number
-      finalValue = totalDegree % 360
-      switch (true) {
-        case ((finalValue >= 0 && finalValue <= 27) || (finalValue >= 316 && finalValue <= 360)):
-          var $drink = $("#drink0")   //trophy           
-          break;
-        case (finalValue >= 28 && finalValue <= 100):
-          var $drink = $("#drink1")   //tint
-          break;
-        case (finalValue >= 101 && finalValue <= 164):
-          var $drink = $("#drink2")   //fire
-          break;
-        case (finalValue >= 165 && finalValue <= 214):
-          var $drink = $("#drink3")   //heart
-          break;
-        case (finalValue >= 215 && finalValue <= 260):
-          var $drink = $("#drink4")   //bolt
-          break;
-        case (finalValue >= 261 && finalValue <= 315):
-          var $drink = $("#drink5")   //star
-          break;
-      }
-      var name = $drink.val();
-      var drink_id = $drink.data('id');
-      var wheel_id = $("#wheel-id").val();
-      var user_id = $("#user-id").val();
-
-      
-      
-      $.post( "wheels/" + wheel_id + "/spin", 
-        { 
-          user_id: user_id, 
-          drink_id: drink_id 
+    revealChoice = setTimeout(function() { // function executes ones after a specified number
+        finalValue = totalDegree % 360
+        switch (true) {
+          case ((finalValue >= 0 && finalValue <= 27) || (finalValue >= 316 && finalValue <= 360)):
+            var $drink = $("#drink0") //trophy           
+            break;
+          case (finalValue >= 28 && finalValue <= 100):
+            var $drink = $("#drink1") //tint
+            break;
+          case (finalValue >= 101 && finalValue <= 164):
+            var $drink = $("#drink2") //fire
+            break;
+          case (finalValue >= 165 && finalValue <= 214):
+            var $drink = $("#drink3") //heart
+            break;
+          case (finalValue >= 215 && finalValue <= 260):
+            var $drink = $("#drink4") //bolt
+            break;
+          case (finalValue >= 261 && finalValue <= 315):
+            var $drink = $("#drink5") //star
+            break;
         }
-      )
-      .done(function() {         // success callback 
-      console.log($drink)             
-        $('#txt').avgrund({
-          width: 200,
-          height: "auto",
-          showClose: true,
-          showCloseText: 'Close',
-          closeByEscape: true,
-          closeByDocument: true,
-          closeByEvent: true,          
-          template: "<div class='thumbnail'> <div class='row'> <div class='col-sm-5'><img src='" + $drink.data('url') +  "'/></div> <div class='col-sm-5'> <div class='caption'> <h3>" + $drink.val() + "</h3> <h6>" + $drink.data('description') + "</h6> </div> </div> </div> </div>"           
-        });  
-        $('#txt').trigger('click')
-      });
-    }, 6000)
-    /*let's make the spin btn to tilt every
-    time the edge of the section hits
-    the indicator*/
+        var name = $drink.val();
+        var drink_id = $drink.data('id');
+        var wheel_id = $("#wheel-id").val();
+        var user_id = $("#user-id").val();
+
+
+
+        $.post("wheels/" + wheel_id + "/spin", {
+            user_id: user_id,
+            drink_id: drink_id
+          })
+          .done(function() { // success callback 
+            console.log($drink)
+            $('#txt').avgrund({
+              width: 200,
+              height: "auto",
+              showClose: true,
+              showCloseText: 'Close',
+              closeByEscape: true,
+              closeByDocument: true,
+              closeByEvent: true,
+              template: "<div class='thumbnail'> <div class='row'> <div class='col-sm-5'><img src='" + $drink.data('url') + "'/></div> <div class='col-sm-5'> <div class='caption'> <h3>" + $drink.val() + "</h3> <h6>" + $drink.data('description') + "</h6> </div> </div> </div> </div>"
+            });
+            $('#txt').trigger('click')
+          });
+      }, 6000)
+      /*let's make the spin btn to tilt every
+      time the edge of the section hits
+      the indicator*/
     $('#wheel .sec').each(function() {
       var t = $(this);
       var noY = 0;
@@ -119,16 +115,14 @@ $(document).ready(function() {
     }); // end btn tilting
   });
 }); //DOCUMENT READY
-	
-	
 
-togglePanes = function(elem1, elem2){
-	console.log("toggle");
+
+
+togglePanes = function(elem1, elem2) {
+  console.log("toggle");
   $(elem1).toggle();
   $(elem2).toggle();
 }
 
 // wheel is spinning.....and when it stops you have a result that is written to #txt
 // #txt 12344.43754745
-
-
