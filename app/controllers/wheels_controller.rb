115aa516adb444c83a15efb357bbf8f4b3356082
kennpangilinan <kenn.pangilinan@gmail.com>
2017-01-02 17:21:51 -0800

    Hero photo edited

--- app/controllers/wheels_controller.rb
+++ app/controllers/wheels_controller.rb
@@ -7,4 +7,46 @@ class WheelsController < ApplicationController
 		puts(params) #create spin event Events::Spin.create(user_id: params[:id], .......)
 		head :ok
 	end
+
+  def show
+    @wheel = Wheel.find(params[:id])
+  end
+
+  def new
+    @wheel = Wheel.new
+  end
+
+  def create
+    @wheel = Wheel.new(params[:wheel]) 
+    if @wheel.save
+      flash[:notice] = "Wheel successfully created!"  
+      redirect_to wheels_path 
+    else
+      render :new 
+    end
+  end 
+
+  def edit
+    @wheel = Wheel.find(params[:id])
+    if @wheel.save
+      redirect_to wheels_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @wheel = Wheel.find(params[:id])
+    if @wheel.update_attributes(params[:wheel])
+      redirect_to wheel_path(@wheel)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @wheel = Wheel.find(params[:id])
+    @wheel.destroy
+    redirect_to wheels_path
+  end
 end
